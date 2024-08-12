f1e19368b5dd1b6f4cea2e810a8ffd1e93c4325b
Caner Ates <cnrates@gmail.com>
2024-08-12 15:52:16 +0300

    Skeleton entry

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
+import { React, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ThemeContext } from "@emotion/react";
+import Main from "./pages/Main"
+import Test from "./pages/Test"
+
+import { lightTheme, darkTheme } from "./utils/Theme";
+
 import './App.css';
 
+
 function App() {
+  const [theme, setTheme] = useState(() => {
+    return window.localStorage.getItem("theme") || "light";
+  });
+
+  const muiTheme = theme === "light" ? lightTheme : darkTheme;
+
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline>
+        <Router>
+            <Routes>
+              <Route exact path="/" element={<Main />} />
+              <Route path="/test" element={<Test />} />
+            </Routes>
+          </Router>
+        </CssBaseline>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ src/utils/Theme.js
@@ -0,0 +1,31 @@
+import { createTheme } from "@mui/material";
+
+export const lightTheme = createTheme({
+    components: {
+        
+    },
+    typography: {
+        
+    },
+    breakpoints: {
+        
+    },
+    palette: {
+        
+    }
+});
+
+export const darkTheme = createTheme({
+    components: {
+
+    },
+    typography: {
+
+    },
+    breakpoints: {
+
+    },
+    palette: {
+
+    }
+});
