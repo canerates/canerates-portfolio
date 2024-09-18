2f835538f15def503184ddc1cf07ef078dccb8b2
Caner Ates <cnrates@gmail.com>
2024-09-18 17:03:20 +0300

    Hero section update

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react"
-import { Container, Grid, Typography, Box, styled } from "@mui/material";
-// import { Typewriter } from 'react-simple-typewriter'
+import { Container, Grid, Typography, Box, styled, useTheme } from "@mui/material";
 import Typewriter from "typewriter-effect";
 import heroImg from "../assets/hero/caner-cropped.png"
 
 const StyledHeroRoot = styled("section")(({ theme }) => ({
   display: "flex",
+  flexWrap: "wrap",
+  alignContent: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "center",
   minHeight: "100vh",
 
 }))
 
 const StyledHeroContainer = styled(Container)(({ theme }) => ({
   display: "flex !important",
+  flex: "1",
+  alignItems: "center",
+  alignSelf: "center",
   boxSizing: "unset !important",
   width: "unset",
+  ["@media (max-height: 804px)"]: {
+    // paddingTop: ismobile ? "0px" : "102px",
+  },
+  [theme.breakpoints.up("sm")]: {
+    marginTop: "2rem",
+  },
 
 }))
 
@@ -21,6 +34,12 @@ const StyledHeroGrid = styled(Grid)(({ theme }) => ({
   justifyContent: "space-around",
   alignItems: "center",
   width: "auto",
+  [theme.breakpoints.down("xs")]: {
+    // maxWidth: "70%",
+    // justifyContent: "flex-end"
+    marginBottom: "2rem",
+    marginTop: "2rem",
+  },
 }));
 
 const StyledHeroGridItem = styled(Grid)(({ theme }) => ({
@@ -28,21 +47,31 @@ const StyledHeroGridItem = styled(Grid)(({ theme }) => ({
   padding: "0rem",
   alignItems: "center",
   justifyContent: "center",
+  [theme.breakpoints.down("xs")]: {
+    order: "1",
+    // marginTop: "2rem",
+    // marginBottom: "2rem"
+  },
 }));
 
 const StyledHeroImage = styled("img")(({ theme }) => ({
   animation: "fadeIn",
   animationDuration: "2s",
-  maxWidth: "100%",
+  maxWidth: "70%",
   height: "auto",
-  // height: "200px",
-  // textAlign: "center",
-  // marginTop: "3rem",
   opacity: theme.palette.heroImage.opacity,
 
+  [theme.breakpoints.down("lg")]: {
+    maxWidth: "70%",
+  },
+  [theme.breakpoints.down("md")]: {
+    maxWidth: "70%",
+  },
+  [theme.breakpoints.down("sm")]: {
+    maxWidth: "95%",
+  },
   [theme.breakpoints.down("xs")]: {
-    width: "60%",
-    // margin: "0",
+    maxWidth: "70%",
   },
 
 }));
@@ -50,44 +79,56 @@ const StyledHeroImage = styled("img")(({ theme }) => ({
 const StyledHeroText = styled(Grid)(({ theme }) => ({
   animation: "fadeIn",
   animationDuration: "2s",
-  // width: "50%",
+  width: "50%",
   paddingLeft: "0 !important",
-  [theme.breakpoints.down("sm")]: {
+  [theme.breakpoints.down("md")]: {
     width: "80%",
     margin: "0",
   },
-  // margin: "2rem 4rem 2rem 4rem ",
+  [theme.breakpoints.down("sm")]: {
+    width: "90%",
+    margin: "0",
+  },
+  [theme.breakpoints.down("xs")]: {
+    width: "100%",
+    margin: "0",
+    order: "2",
+    textAlign: "center",
+    padding: "2rem",
+    paddingRight: "0 !important"
+  },
 }));
 
 const StyledHeroGreetingText = styled(Typography)(({ theme }) => ({
   fontWeight: "bold !important",
   textAlign: "left",
   color: theme.palette.text.primary,
-  fontSize: "clamp(20px, 1.5vw, 26px) !important",
-  // marginBottom: theme.spacing(0),
+  fontSize: "clamp(20px, 2vw, 26px) !important",
 
-  // [theme.breakpoints.up("md")]: {
-  //   whiteSpace: "nowrap",
-  // },
+  [theme.breakpoints.down("xs")]: {
+    textAlign: "center"
+  },
 }));
 
-const StyledHeroTitle = styled("div")(({ theme }) => ({
-  textAlign: "justify",
+const StyledHeroTitle = styled(Typography)(({ theme }) => ({
   fontWeight: "bold !important",
   color: theme.palette.text.primary,
-  fontSize: "clamp(28px, 3vw, 48px) !important",
-  marginTop: "0.1rem",
-  marginBottom: "0.5rem",
-  // [theme.breakpoints.up("md")]: {
-  //   whiteSpace: "nowrap",
-  // },
+  marginBottom: theme.spacing(0),
+  fontSize: "clamp(34px, 4vw, 55px) !important",
+  [theme.breakpoints.up("md")]: {
+    whiteSpace: "nowrap",
+  },
+  [theme.breakpoints.down("xs")]: {
+    textAlign: "center"
+  },
 }));
 
 const StyledHeroSubText = styled(Typography)(({ theme }) => ({
   color: theme.palette.text.secondary + " !important",
-  textAlign: "left",
-  fontSize: "clamp(16px, 1.5vw, 22px) !important",
-  fontStyle: "italic"
+  fontSize: "clamp(20px, 2vw, 26px) !important",
+  [theme.breakpoints.down("xs")]: {
+    textAlign: "center"
+  },
 }));
 
 const StyledResumeLink = styled("a")(({ theme }) => ({
@@ -109,6 +150,18 @@ const StyledResumeLink = styled("a")(({ theme }) => ({
 }));
 
 const Hero = ({ img }) => {
+
+  const theme = useTheme();
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobileDevice =
+      /mobile|android|ios|iphone|ipad|ipod|windows phone/i.test(userAgent);
+    setIsMobile(isMobileDevice);
+  }, []);
+
   return (
     <StyledHeroRoot>
       <StyledHeroContainer>
@@ -116,10 +169,12 @@ const Hero = ({ img }) => {
           <StyledHeroText
             item
             xxs={12}
-            xs={4}
+            xs={7}
+            sm={7}
+            md={7}
           >
             <StyledHeroGreetingText>
-              Hello, I am
+              Hello, I'm
             </StyledHeroGreetingText>
             <StyledHeroTitle>
               <Typewriter
@@ -138,6 +193,8 @@ const Hero = ({ img }) => {
               Designing and developing<br /> native iOS applications..
             </StyledHeroSubText>
 
+
+
             <StyledResumeLink
               href={
                 process.env.PUBLIC_URL + "/Caner Ates Resume iOS Dev 2024-5.pdf"
@@ -147,24 +204,18 @@ const Hero = ({ img }) => {
               <Typography>Resume</Typography>
             </StyledResumeLink>
 
+
           </StyledHeroText>
-
-
 
           <StyledHeroGridItem
             item
             xxs={12}
-            xs={4}
+            xs={5}
+            sm={5}
+            md={5}
           >
             <StyledHeroImage alt="Image of Caner Ates" src={heroImg} />
           </StyledHeroGridItem>
-
-          <StyledHeroText
-            item
-            xxs={12}
-            xs={4}
-          >
-          </StyledHeroText>
 
         </StyledHeroGrid>
       </StyledHeroContainer>
